11c821163a2d44716525104bcb5e1c2552df33da
Yoginth <hey@yoginth.com>
2025-11-20 18:34:29 +0530

    refactor: consolidate file size upload limits in attachmentUtils to a single maximum limit for improved maintainability

--- apps/web/src/helpers/attachmentUtils.ts
+++ apps/web/src/helpers/attachmentUtils.ts
@@ -4,32 +4,30 @@ import { toast } from "sonner";
 import compressImage from "./compressImage";
 
 const BYTES_IN_MB = 1_000_000;
-const IMAGE_UPLOAD_LIMIT = 50000000;
-const VIDEO_UPLOAD_LIMIT = 2000000000;
-const AUDIO_UPLOAD_LIMIT = 600000000;
+const MAX_UPLOAD_LIMIT = 8 * BYTES_IN_MB;
 
 export const validateFileSize = (file: File): boolean => {
   const isImage = file.type.includes("image");
   const isVideo = file.type.includes("video");
   const isAudio = file.type.includes("audio");
 
-  if (isImage && file.size > IMAGE_UPLOAD_LIMIT) {
+  if (isImage && file.size > MAX_UPLOAD_LIMIT) {
     toast.error(
-      `Image size should be less than ${IMAGE_UPLOAD_LIMIT / BYTES_IN_MB}MB`
+      `Image size should be less than ${MAX_UPLOAD_LIMIT / BYTES_IN_MB}MB`
     );
     return false;
   }
 
-  if (isVideo && file.size > VIDEO_UPLOAD_LIMIT) {
+  if (isVideo && file.size > MAX_UPLOAD_LIMIT) {
     toast.error(
-      `Video size should be less than ${VIDEO_UPLOAD_LIMIT / BYTES_IN_MB}MB`
+      `Video size should be less than ${MAX_UPLOAD_LIMIT / BYTES_IN_MB}MB`
     );
     return false;
   }
 
-  if (isAudio && file.size > AUDIO_UPLOAD_LIMIT) {
+  if (isAudio && file.size > MAX_UPLOAD_LIMIT) {
     toast.error(
-      `Audio size should be less than ${AUDIO_UPLOAD_LIMIT / BYTES_IN_MB}MB`
+      `Audio size should be less than ${MAX_UPLOAD_LIMIT / BYTES_IN_MB}MB`
     );
     return false;
   }
@@ -42,7 +40,7 @@ export const compressFiles = async (files: File[]): Promise<File[]> => {
     files.map(async (file) => {
       if (file.type.includes("image") && !file.type.includes("gif")) {
         return await compressImage(file, {
-          maxSizeMB: 9,
+          maxSizeMB: 8,
           maxWidthOrHeight: 6000
         });
       }
