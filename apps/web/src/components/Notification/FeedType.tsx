bf319803b45ec8a0617104fa11eeb05cd95b4f6d
Paul Burke <paul@paulburke.co>
2025-12-20 22:59:03 -0500

    feat: uses search params for notification tabs

--- apps/web/src/components/Notification/FeedType.tsx
+++ apps/web/src/components/Notification/FeedType.tsx
@@ -1,5 +1,6 @@
 import { NotificationFeedType } from "@palus/data/enums";
-import type { Dispatch, SetStateAction } from "react";
+import { type Dispatch, type SetStateAction, useEffect } from "react";
+import { useSearchParams } from "react-router";
 import { Tabs } from "@/components/Shared/UI";
 
 interface FeedTypeProps {
@@ -17,14 +18,26 @@ const FeedType = ({ feedType, setFeedType }: FeedTypeProps) => {
     { name: "Rewards", type: NotificationFeedType.Rewards }
   ];
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const tab = searchParams.get("type");
+
+  useEffect(() => {
+    if (!tab) return;
+    setFeedType(tab.toUpperCase() as NotificationFeedType);
+  }, [tab]);
+
   return (
     <Tabs
       active={feedType}
       className="mx-5 mb-5 md:mx-0"
       layoutId="notification_tab"
       setActive={(type) => {
-        const nextType = type as NotificationFeedType;
-        setFeedType(nextType);
+        setFeedType(type as NotificationFeedType);
+        setSearchParams(
+          type !== NotificationFeedType.All
+            ? `type=${type.toLowerCase()}`
+            : undefined
+        );
       }}
       tabs={tabs}
     />
