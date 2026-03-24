4ac3a602a7d17fb0b05704dc52fb1548fcf597ee
Paul Burke <paul@paulburke.co>
2026-03-24 09:50:29 -0400

    feat: only polls notifications when tab is visible

--- packages/web/src/hooks/useHasNewNotifications.ts
+++ packages/web/src/hooks/useHasNewNotifications.ts
@@ -1,5 +1,7 @@
 import { useNotificationIndicatorQuery } from "@palus/indexer";
+import { useVisibilityChange } from "@uidotdev/usehooks";
 import { getNotificationTimestamp } from "@/helpers/getNotificationTimestamp";
+import { useIntervalWhen } from "@/hooks/useIntervalWhen";
 import { useAccountStore } from "@/store/persisted/useAccountStore";
 import { useNotificationStore } from "@/store/persisted/useNotificationStore";
 import { usePreferencesStore } from "@/store/persisted/usePreferencesStore";
@@ -8,12 +10,18 @@ const useHasNewNotifications = () => {
   const { currentAccount } = useAccountStore();
   const { lastSeenNotificationTimestamp } = useNotificationStore();
   const { includeLowScore } = usePreferencesStore();
+  const documentVisible = useVisibilityChange();
 
-  const { data } = useNotificationIndicatorQuery({
+  const { data, refetch } = useNotificationIndicatorQuery({
     fetchPolicy: "no-cache",
-    pollInterval: 60 * 1000,
     skip: !currentAccount,
     variables: { request: { filter: { includeLowScore } } }
+  });
+
+  useIntervalWhen(refetch, {
+    ms: 60 * 1000,
+    startImmediately: true,
+    when: documentVisible
   });
 
   const latestNotification = data?.notifications?.items[0];
