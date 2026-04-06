a89c7440201dca553189e0e404c52f4f018754c6
Paul Burke <paul@paulburke.co>
2026-04-05 21:16:18 -0400

    fix: only shows post modal from share target when logged in

--- packages/web/src/hooks/useShareTargetListener.tsx
+++ packages/web/src/hooks/useShareTargetListener.tsx
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 import { useSearchParams } from "react-router";
 import { useNewPostModalStore } from "@/store/non-persisted/modal/useNewPostModalStore";
 import { usePostStore } from "@/store/non-persisted/post/usePostStore";
+import { useAccountStore } from "@/store/persisted/useAccountStore";
 
 const useShareTargetListener = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const { setShow: setShowNewPostModal } = useNewPostModalStore();
   const { setPostContent } = usePostStore();
+  const { currentAccount } = useAccountStore();
 
   useEffect(() => {
+    if (!currentAccount) return;
+
     if (searchParams.has("text")) {
       const text = searchParams.get("text");
       const url = searchParams.get("url");
@@ -17,7 +21,7 @@ const useShareTargetListener = () => {
       setPostContent(content);
       setSearchParams();
     }
-  }, [searchParams]);
+  }, [searchParams, currentAccount]);
 };
 
 export default useShareTargetListener;
