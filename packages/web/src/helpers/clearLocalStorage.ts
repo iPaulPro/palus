74df6bd727a4fb6d1d4ae532c4de43c34549aab7
Paul Burke <paul@paulburke.co>
2026-04-24 10:43:40 -0400

    feat: stops clearing preferences on log out

--- packages/web/src/helpers/clearLocalStorage.ts
+++ packages/web/src/helpers/clearLocalStorage.ts
@@ -2,7 +2,9 @@ import { Localstorage } from "@/data/storage";
 
 const clearLocalStorage = (): void => {
   const storesToClear = Object.values(Localstorage).filter(
-    (store) => store !== Localstorage.SearchStore
+    (store) =>
+      store !== Localstorage.SearchStore &&
+      store !== Localstorage.PreferencesStore
   );
 
   for (const store of storesToClear) {
