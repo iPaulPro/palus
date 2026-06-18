493a06c18271c5e17800bd73cc99fe6464ab49ae
Paul Burke <paul@paulburke.co>
2026-06-18 12:39:18 -0400

    fix: SelectUI chevron color

--- packages/web/src/components/Shared/UI/SelectUI.tsx
+++ packages/web/src/components/Shared/UI/SelectUI.tsx
@@ -47,7 +47,7 @@ function SelectTrigger({
     >
       {children}
       <SelectPrimitive.Icon asChild>
-        <ChevronDownIcon className="size-4 opacity-50" />
+        <ChevronDownIcon className="size-4 text-secondary" />
       </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
   );
